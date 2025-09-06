55b609be878065742438bf395f703bdfff23ad35
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2025-09-06 23:46:59 +0000

    Update errors.go

--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -7,8 +7,5 @@ import (
 var (
 	EmptyDirectory   = fmt.Errorf("a directory path was blank")
 	InvalidDirectory = func(msg string) error { return fmt.Errorf("invalid directory: %s", msg) }
+	VersionMismatch  = func(got, expected int) { return fmt.Errorf("configuration version mismatch. got %d, expected %d", got, expected) }
 )
-
-func VersionMismatch(got, expected int) error {
-	return fmt.Errorf("configuration version mismatch. got %d, expected %d", got, expected)
-}
